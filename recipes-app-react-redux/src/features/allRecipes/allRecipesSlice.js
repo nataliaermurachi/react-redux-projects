702c174b7e31a6c9e49bb8353f871d24d1719d2d
Natalia Ermurachi <ermurachinatalia5@gmail.com>
2021-04-22 16:15:51 +0200

    add selector functions and test them

--- recipes-app-react-redux/src/features/allRecipes/allRecipesSlice.js
+++ recipes-app-react-redux/src/features/allRecipes/allRecipesSlice.js
@@ -1,4 +1,5 @@
 import allRecipesData from '../../data';
+import { selectSearchTerm } from '../searchTerm/searchTermSlice.js';
 
 export const loadData = () => {
   return {
@@ -20,3 +21,31 @@ export const allRecipesReducer = (allRecipes = initialState, action) => {
       return allRecipes;
   }
 }
+
+// Implement the selectors below.
+export const selectAllRecipes = state => state.allRecipes;
+export const selectFilteredAllRecipes = state => {
+  const allRecipes = selectAllRecipes(state);
+  const searchTerm = selectSearchTerm(state);
+  return allRecipes.filter(recipe => recipe.name.toLowerCase().includes(searchTerm.toLowerCase()))
+};
+
+// This code is for testing the seletors only.
+const testState = {
+  allRecipes: allRecipesData,
+  searchTerm: 'ch'
+}
+
+const testSelectAllRecipes = () => {
+  console.log('All Recipes')
+  console.log(selectAllRecipes(testState));
+}
+
+const testSelectFilteredAllRecipes = () => {
+  console.log('\nRecipes filtered by searchTerm')
+  console.log(selectFilteredAllRecipes(testState));
+}
+
+// Uncomment these to test each selector.
+testSelectAllRecipes();
+testSelectFilteredAllRecipes(); 

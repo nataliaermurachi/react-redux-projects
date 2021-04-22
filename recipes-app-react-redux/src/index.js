3938d69e3ec0c0fe7d62ba789e974a4f5e814423
Natalia Ermurachi <ermurachinatalia5@gmail.com>
2021-04-22 15:49:24 +0200

    Add react-redux module, add provider component,delete render method and subscribe

--- recipes-app-react-redux/src/index.js
+++ recipes-app-react-redux/src/index.js
@@ -3,21 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './app/App.js';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+
 import { store } from './app/store';
 
-const render = () => {
-  // Pass `state` and `dispatch` props to <App />
+
   ReactDOM.render(
-    <React.StrictMode>
-      <App state={store.getState()} dispatch={store.dispatch}/>
-    </React.StrictMode>,
+    <Provider store={store}>
+      <React.StrictMode>
+        <App state={store.getState()} dispatch={store.dispatch}/>
+      </React.StrictMode>
+    </Provider>
+    ,
     document.getElementById('root')
   );
-}
-render();
-// Subscribe render to changes to the `store`
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
-store.subscribe(render);
